503644fb75aa20a9d31ab8b094982dc52e8c3e44
mandeep <mandip10@gmail.com>
2017-08-29 10:45:57 -0500

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { ProductListComponent  } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipes';
+import { AppComponent } from './app.component';
+import { StarComponent } from './shared/star.component';
+import { WelcomeComponent } from './home/welcome.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    WelcomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
