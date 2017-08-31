55289978368831555b5ffecec27a5c9b1170c0e4
mandeep <mandip10@gmail.com>
2017-08-31 11:03:12 -0500

    product detail component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent  } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipes';
 import { AppComponent } from './app.component';
 import { StarComponent } from './shared/star.component';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
@@ -13,11 +15,13 @@ import { WelcomeComponent } from './home/welcome.component';
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   
   providers: [],
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class ProductListComponent implements OnInit{
     imageWidth: number=50;
     imageMargin: number=2;
     showImage: boolean=false;
+    errorMessage: string;
 
     _listFilter: string;
     get listFilter(): string{
@@ -23,33 +25,10 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }
+    products: IProduct[] = [];
 
-    ];
-
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = '';
+    constructor(private _productService: ProductService){
+        
     }
 
     onRatingClicked(message: string): void {
@@ -67,6 +46,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void {
-        console.log('In OnInit');
+        this._productService.getProducts()
+            .subscribe(products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+                    
+            },
+        error => this.errorMessage = <any>error);
     }
 }
